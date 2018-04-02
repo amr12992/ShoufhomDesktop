53a40f1620afade77cff104cc734c46567929b4c
amr12992 <36440699+amr12992@users.noreply.github.com>
2018-04-02 04:22:43 +0300

    Added functionality for class students

--- js/classes_students.js
+++ js/classes_students.js
@@ -1,67 +1,70 @@
 var SERVER_URL = "http://ShoufhomNode-iam688687.codeanyapp.com:3000"
 
 $(document).ready(function() {
+    $("#datetimepicker").shieldDateTimePicker({
+
+    });
     $("#grid").shieldGrid({
         dataSource: {
-        remote: {
-            read: {
-                url: SERVER_URL + "/readstudents",
-                type: "post"
+            remote: {
+                read: {
+                    url: SERVER_URL + "/getClasses",
+                    type: "post"
+                },
+                modify: {
+                    create: {
+                        url: SERVER_URL + "/addClass",
+                        type: "post",
+                        data: function (edited) {
+                            return edited[0].data;
+                        }
+                    },
+                    update: {
+                        url: SERVER_URL + "/modifyClass",
+                        type: "post",
+                        data: function (edited) {
+                            return edited[0].data;
+                        }
+                    },
+                    remove: {
+                        url: SERVER_URL + "/deleteClass",
+                        type: "post",
+                        data: function (removed) {
+                            return removed[0].data;
+                        }
+                    }
+                }
             },
-            modify: {
-                create: {
-                    url: SERVER_URL + "/editstudents",
-                    type: "post",
-                    data: function (edited) {
-                        return edited;
-                    }
-                },
-                update: {
-                    url: SERVER_URL + "/editstudents",
-                    type: "post",
-                    data: function (edited) {
-                        return edited[0].data;
-                    }
-                },
-                remove: {
-                    url: SERVER_URL + "/removestudent",
-                    type: "post",
-                    data: function (removed) {
-                        return { ID: removed[0].data.studentID };
-                    }
+            schema: {
+                fields: {
+                    gradeLevel: { path: "gradeLevel", type: String },
+                    classNumber: { path: "classNumber", type: String },
+                    termYear: { path: "termYear" },
+                    classID: { path: "classID" },
                 }
             }
         },
-        schema: {
-            fields: {
-                studentID: { path: "studentID", type: String },
-                fName: { path: "fName", type: String },
-                mName: { path: "mName", type: String },
-                lName: { path: "lName", type: String },
-            }
-        }
-    },
         paging: {
             pageSize: 20,
         },
         events: {
-            detailCreated: subjects
+            detailCreated: students
         },
         rowHover: false,
         columns: [{
-            field: "studentID",
-            title: "Student ID",
-            width: "60px"
+            field: "classID",
+            title: "Class ID"
         }, {
-            field: "fName",
-            title: "First Name",
-            width: "120px"
+            field: "termYear",
+            title: "Year"
         }, {
-            field: "lName",
-            title: "Last Name",
-            width: "120px"
+            field: "gradeLevel",
+            title: "Class level"
         }, {
-            width: "60px",
+            field: "classNumber",
+            title: "Section"
+        }, {
+            width: "140px",
             title: "Options",
             buttons: [{ commandName: "edit", caption: "Edit" },
             { commandName: "delete", caption: "Delete" }
@@ -75,117 +78,117 @@ $(document).ready(function() {
             "delete": {
                 enabled: true,
                 template: function(item) {
-                    return "Delete student ID " + item.studentID
+                    return "Delete class ID " + item.classID
                 }
             }
         }
     },
     toolbar: [
-                {
-                    buttons: [
-                        { commandName: "insert", caption: "Add student" }
-                    ],
-                    position: "bottom"
-                }
-            ]
+        {
+            buttons: [
+                { commandName: "insert", caption: "Add class" }
+            ],
+            position: "top"
+        }
+    ]
 });
 
-function subjects(e) {
+function students(e) {
     var request = {
-      studentID: e.item.studentID
+        classID: e.item.classID
     };
     $("<div/>").appendTo(e.detailCell).shieldGrid({
-      dataSource: {
-      remote: {
-          read: {
-              url: SERVER_URL + "/getStudentSubjects",
-              type: "post",
-              data: function() {
-                return request;
-              }
-          },
-          modify: {
-              create: {
-                  url: SERVER_URL + "/editstudents",
-                  type: "post",
-                  data: function (edited) {
-                      return edited;
-                  }
-              },
-              update: {
-                  url: SERVER_URL + "/editstudents",
-                  type: "post",
-                  data: function (edited) {
-                      return edited[0].data;
-                  }
-              },
-              remove: {
-                  url: SERVER_URL + "/removestudent",
-                  type: "post",
-                  data: function (removed) {
-                      return { ID: removed[0].data.studentID };
-                  }
-              }
-          }
-      },
-      schema: {
-          fields: {
-              subjectName: { path: "subjectName", type: String },
-              subjectID: { path: "subjectID", type: String },
-              fName: { path: "fName", type: String },
-              mName: { path: "mName", type: String },
-              lName: { path: "lName", type: String },
-          }
-      }
-  },
-      paging: {
-          pageSize: 20,
-      },
-      events: {
+        dataSource: {
+            remote: {
+                read: {
+                    url: SERVER_URL + "/getStudentsFromClass",
+                    type: "post",
+                    data: function() {
+                        return request;
+                    }
+                },
+                modify: {
+                    create: {
+                        url: SERVER_URL + "/addStudentToClass",
+                        type: "post",
+                        data: function (edited) {
+                            edited[0].data.classID = request.classID;
+                            return edited[0].data;
+                        }
+                    },
+                    remove: {
+                        url: SERVER_URL + "/deleteStudentFromClass",
+                        type: "post",
+                        data: function (removed) {
+                            removed[0].data.classID = request.classID;
+                            return removed[0].data;
+                        }
+                    }
+                }
+            },
+            schema: {
+                fields: {
+                    studentID: { path: "studentID", type: String },
+                    fName: { path: "fName" },
+                    mName: { path: "mName" },
+                    lName: { path: "lName" },
+                }
+            },
+            sorting: {
+                multiple: true
+            }
+        },
+        paging: {
+            pageSize: 20,
+        },
+        events: {
 
-      },
-      rowHover: false,
-      columns: [{
-          field: "subjectName",
-          title: "Subject Name",
-          width: "120px"
-      }, {
-          field: "fName",
-          title: "Teacher First Name",
-          width: "120px"
-      }, {
-          field: "lName",
-          title: "Teacher Last Name",
-          width: "120px"
-      }, {
-          width: "60px",
-          title: "Options",
-          buttons: [{ commandName: "edit", caption: "Edit" },
-          { commandName: "delete", caption: "Delete" }
-      ]
-  }],
-  editing: {
-      enabled: true,
-      type: "cell",
-      mode: "form",
-      confirmation: {
-          "delete": {
-              enabled: true,
-              template: function(item) {
-                  return "Delete student ID " + item.studentID
-              }
-          }
-      }
-  },
-  toolbar: [
-              {
-                  buttons: [
-                      { commandName: "insert", caption: "Add student" }
-                  ],
-                  position: "bottom"
-              }
-          ]
-    });
+        },
+        rowHover: false,
+        columns: [{
+            field: "studentID",
+            title: "Student ID"
+        }, {
+            field: "fName",
+            title: "Student First Name"
+        }, {
+            field: "lName",
+            title: "Student Last Name"
+        }, {
+            width: "150px",
+            title: "Options",
+            buttons: [{ commandName: "edit", caption: "Edit" },
+            { commandName: "delete", caption: "Remove" }
+        ]
+    }],
+    editing: {
+        enabled: true,
+        type: "cell",
+        mode: "form",
+        confirmation: {
+            "delete": {
+                enabled: true,
+                template: function(item) {
+                    return "Remove student ID " + item.studentID + " from class?";
+                }
+            }
+        }
+    },
+    toolbar: [
+        {
+            buttons: [
+                { commandName: "insert", caption: "Add student" }
+            ],
+            position: "top"
+        }
+    ]
+});
+}
+
+function timePicker(cell, item) {
+    $('<div id="dropdown"/>')
+    .appendTo(cell)
+    .shieldDateTimePicker().swidget().focus();
 }
 
 function myCustomEditor(cell, item) {
