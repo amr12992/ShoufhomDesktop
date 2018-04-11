b610ccea70514c402e8dc1f5026409b7d4841191
amr12992 <36440699+amr12992@users.noreply.github.com>
2018-04-11 09:54:14 +0300

    fully functional terms

--- js/terms.js
+++ js/terms.js
@@ -7,23 +7,11 @@ $(document).ready(function() {
                 read: {
                     url: SERVER_URL + "/readterms",
                     type: "post"
-                },
-                modify: {
-                    create: {
-                        url: SERVER_URL + "/createnewterm",
-                        type: "post",
-                        data: function (edited) {
-                            return edited[0].data;
-                        }
-                    }
                 }
             },
             schema: {
                 fields: {
-                    gradeLevel: { path: "gradeLevel", type: String },
-                    classNumber: { path: "classNumber", type: String },
                     termYear: { path: "termYear" },
-                    classID: { path: "classID" },
                 }
             }
         },
@@ -31,227 +19,27 @@ $(document).ready(function() {
             pageSize: 20,
         },
         events: {
-            
-        },
-        rowHover: false,
-        columns: [{
-            field: "classID",
-            title: "Class ID"
-        }, {
-            width: "140px",
-            title: "Options",
-            buttons: [{ commandName: "edit", caption: "Edit" },
-            { commandName: "delete", caption: "Delete" }
-        ]
-    }],
-    editing: {
-        enabled: true,
-        type: "cell",
-        mode: "form",
-        confirmation: {
-            "delete": {
-                enabled: true,
-                template: function(item) {
-                    return "Delete class ID " + item.classID
-                }
-            }
-        }
-    },
-    toolbar: [
-        {
-            buttons: [
-                { commandName: "insert", caption: "Add class" }
-            ],
-            position: "top"
-        }
-    ]
-});
 
-function subjects(e) {
-    var request = {
-        classID: e.item.classID
-    };
-    $("<div/>").appendTo(e.detailCell).shieldGrid({
-        dataSource: {
-            remote: {
-                read: {
-                    url: SERVER_URL + "/getSubjectsOfClass",
-                    type: "post",
-                    data: function() {
-                        return request;
-                    }
-                },
-                modify: {
-                    create: {
-                        url: SERVER_URL + "/addSubject",
-                        type: "post",
-                        data: function (edited) {
-                            edited[0].data.classID = request.classID;
-                            return edited[0].data;
-                        }
-                    },
-                    update: {
-                        url: SERVER_URL + "/modifySubject",
-                        type: "post",
-                        data: function (edited) {
-                            edited[0].data.classID = request.classID;
-                            return edited[0].data;
-                        }
-                    },
-                    remove: {
-                        url: SERVER_URL + "/deleteSubject",
-                        type: "post",
-                        data: function (removed) {
-                            return removed[0].data;
-                        }
-                    }
-                }
-            },
-            schema: {
-                fields: {
-                    subjectName: { path: "subjectName", type: String },
-                    subjectID: { path: "subjectID" },
-                    subjectType: { path: "subjectType", type: String },
-                    teacherID: { path: "userID", type: String },
-                    fName: { path: "fName" },
-                    mName: { path: "mName" },
-                    lName: { path: "lName" },
-                }
-            },
-            sorting: {
-                multiple: true
-            }
-        },
-        paging: {
-            pageSize: 20,
-        },
-        events: {
-            detailCreated: examTimes
         },
         rowHover: false,
         columns: [{
-            field: "subjectID",
-            title: "Subject ID"
-        }, {
-            field: "subjectName",
-            title: "Subject Name"
-        }, {
-            field: "subjectType",
-            title: "Subject Type",
-        }, {
-            field: "teacherID",
-            title: "Teacher ID",
-        }, {
-            field: "fName",
-            title: "Teacher First Name"
-        }, {
-            field: "lName",
-            title: "Teacher Last Name"
-        }, {
-            width: "150px",
-            title: "Options",
-            buttons: [{ commandName: "edit", caption: "Edit" },
-            { commandName: "delete", caption: "Delete" }
-        ]
-    }],
-    editing: {
-        enabled: true,
-        type: "cell",
-        mode: "form",
-        confirmation: {
-            "delete": {
-                enabled: true,
-                template: function(item) {
-                    return "Delete subject ID " + item.subjectID
-                }
-            }
-        }
-    },
-    toolbar: [
-        {
-            buttons: [
-                { commandName: "insert", caption: "Add subject" }
-            ],
-            position: "top"
-        }
-    ]
-});
-}
-
-function examTimes(e) {
-    var request = {
-        subjectID: e.item.subjectID
-    };
-    $("<div/>").appendTo(e.detailCell).shieldGrid({
-        dataSource: {
-            remote: {
-                read: {
-                    url: SERVER_URL + "/getSubjectExams",
-                    type: "post",
-                    data: function() {
-                        return request;
-                    }
-                },
-                modify: {
-                    update: {
-                        url: SERVER_URL + "/modifySubjectExams",
-                        type: "post",
-                        data: function (edited) {
-                            var data = edited[0].data;
-                            return {
-                              subjectID: data.subjectID,
-                              dateTime: data.date + ' ' + data.time,
-                              exam: data.exam
-                            };
-                        }
-                    }
-                }
-            },
-            schema: {
-                fields: {
-                    subjectID: { path: "subjectID", type: String },
-                    date: { path: "date", type: String },
-                    time: { path: "time", type: String },
-                    exam: { path: "exam" }
-                }
-            }
-        },
-        events: {
-        },
-        rowHover: false,
-        columns: [{
-            field: "date",
-            title: "Date"
-        }, {
-            field: "time",
-            title: "Time"
-        }, {
-            width: "80px",
-            title: "Options",
-            buttons: [{ commandName: "edit", caption: "Edit" }]
+            field: "termYear",
+            title: "Term year"
         }],
-        editing: {
-            enabled: true,
-            type: "cell",
-            mode: "form"
+    editing: {
+        enabled: true,
+        type: "cell",
+        mode: "form",
+        confirmation: {
         }
-    });
-}
-
-function timePicker(cell, item) {
-    $('<div id="dropdown"/>')
-    .appendTo(cell)
-    .shieldDateTimePicker().swidget().focus();
-}
-
-function myCustomEditor(cell, item) {
-    $('<div id="dropdown"/>')
-    .appendTo(cell)
-    .shieldDropDown({
-        dataSource: {
-            data: ["motorbike", "car", "truck"]
-        },
-        value: !item["transport"] ? null : item["transport"].toString()
-    }).swidget().focus();
-}
+    }
 });
+});
+
+function addNewTerm() {
+    var confirmed = confirm("WARNING: Starting a new term will effectively archive ALL the data from the current term, meaning they cannot be altered anymore.\n\nProceed?");
+    if (confirmed)
+        $.post(SERVER_URL + '/createNewTerm', function(data) {
+            location.reload();
+        });
+}
